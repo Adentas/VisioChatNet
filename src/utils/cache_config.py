d46c900ee28c210884b978c683231480ec0be729
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2024-03-18 21:31:20 +0100

    fix bug
    fixed bug with login_message; fixed redis url;

--- src/utils/cache_config.py
+++ src/utils/cache_config.py
@@ -3,7 +3,7 @@ from flask_caching import Cache
 cache = Cache(config = {
     'CACHE_TYPE': 'redis',
     'CACHE_DEFAULT_TIMEOUT': 300, 
-    'CACHE_REDIS_URL': 'redis://localhost:6379/0',
+    'CACHE_REDIS_URL': 'redis://redis:6379/0',
     }
 )
 
